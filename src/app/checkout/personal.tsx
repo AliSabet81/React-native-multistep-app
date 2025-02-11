91ec60f27c44afe5de2eec93d3cce65ecd4f884d
AliSabet81 <sabet.farshid1381@gmail.com>
2025-02-11 11:52:27 +0330

    custom textinput

--- src/app/checkout/personal.tsx
+++ src/app/checkout/personal.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 import CustomButton from "../../components/CustomButton";
 import { router } from "expo-router";
+import CustomTextInput from "../../components/CustomTextInput";
 
 const PersonalDetailsForm = () => {
   const onNext = () => {
@@ -13,7 +14,32 @@ const PersonalDetailsForm = () => {
 
   return (
     <View style={styles.container}>
-      <Text>PersonalDetailsForm</Text>
+      <CustomTextInput name="fullName" label="Full name" placeholder="Joe do" />
+
+      <CustomTextInput name="address" label="Address" placeholder="Address" />
+
+      <View style={{ flexDirection: "row", gap: 5 }}>
+        <CustomTextInput
+          name="city"
+          label="City"
+          placeholder="City"
+          containerStyle={{ flex: 1 }}
+        />
+        <CustomTextInput
+          name="postcode"
+          label="Post code"
+          placeholder="1234"
+          containerStyle={{ flex: 1 }}
+        />
+      </View>
+
+      <CustomTextInput
+        name="phone"
+        label="Phone number"
+        placeholder="601234123123"
+        inputMode="tel"
+      />
+
       <CustomButton title="Next" onPress={onNext} style={styles.button} />
     </View>
   );
@@ -22,8 +48,9 @@ export default PersonalDetailsForm;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     padding: 10,
+    gap: 5,
   },
   button: {
     marginTop: "auto",
