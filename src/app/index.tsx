bc096653878ca0985131b1eb867812c58d08dfe8
AliSabet81 <sabet.farshid1381@gmail.com>
2025-02-11 10:23:27 +0330

    start

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: "Home" }} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
